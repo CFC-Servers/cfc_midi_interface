263d198ff12b7d3f9ac19b6dcacb3e6fdbf3de4f
Samuel Williams <williamssamuel1999@gmail.com>
2020-02-05 19:21:25 +0000

    Base files

---
+++ lua/autorun/server/sv_cfc_mi_init.lua
@@ -0,0 +1,5 @@
+CreateConVar( "sv_midi_sf_notes_quota", 30, FCVAR_REPLICATED + FCVAR_ARCHIVE + FCVAR_PROTECTED )
+AddCSLuaFile("cfc_midi_interface/base.lua")
+AddCSLuaFile("cfc_midi_interface/key_lookup.lua")
+AddCSLuaFile("cfc_midi_interface/starfall_midi.lua")
+AddCSLuaFile("cfc_midi_interface/console.lua")
---
+++ lua/cfc_midi_interface/starfall_midi.lua
@@ -0,0 +1,69 @@
+-- Adds SF hook call, and ability to send key events to an instrument
+hook.Add("Initialize", "MIDI_SF", function()
+	if not SF then return end
+	SF.hookAdd("MIDI")
+	local ent_methods = SF.Entities.Methods
+	local checktype = SF.CheckType
+
+	function ent_methods:isInstrument()
+		SF.CheckType(self, SF.Entities.Metatable)
+		local e = SF.Entities.Unwrap(self)
+		return e.OnRegisteredKeyPlayed and true or false
+	end
+
+	-- We want to limit playNote calls as it directly calls net.Send, and could be used to lag the server.
+	local limitCVar = GetConvar("sv_midi_max_notes_per_second")
+	local limit = limitCVar:GetInt()
+	cvars.AddChangeCallback("sv_midi_sf_notes_quota", function(_, newLimit)
+		limit = newLimit
+	end )
+
+	local noteCount = 0
+	local timerActive = false
+	local function createTimer()
+		if not timerActive then
+			timerActive = true
+			timer.Create("cfc_mi_sfnotelimit", 1, 0, function()
+				if noteCount == 0 then
+					timer.Remove("cfc_mi_sfnotelimit")
+					timerActive = false
+				end
+				noteCount = 0
+			end )
+		end
+	end
+
+	-- boolean Entity:playNote( number noteIdx )
+	-- Takes note index from 1 to 61
+	-- Returns if successful or not
+	-- Limited by sv_midi_sf_notes_quota as a maximum number of notes per second
+	function ent_methods:playNote(noteIdx)
+		SF.CheckType(self, SF.Entities.Metatable)
+		SF.CheckLuaType(noteIdx, TYPE_NUMBER)
+		local ent = SF.Entities.Unwrap(self)
+		if not ent.OnRegisteredKeyPlayed then
+			error("Entity is not an instrument.")
+		end
+		if FPP then
+			if not FPP.canTouchEnt(ent, "Physgun") then
+				error("You do not have permission to send notes to this instrument")
+			end
+		else
+			local o = ent:GetOwner()
+			if IsValid(o) then
+				if o ~= LocalPlayer() then
+					error("You do not have permission to send notes to this instrument")
+				end
+			end
+		end
+
+		createTimer()
+		noteCount = noteCount + 1
+		if noteCount > limit then
+			return false -- Called too many times
+		end
+
+		cfc_midi.sendNote(ent, noteIdx)
+		return true
+	end
+end)
